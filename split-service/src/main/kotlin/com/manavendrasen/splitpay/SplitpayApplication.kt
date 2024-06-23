352c7acbf5701b42b533e3c77150d029b3811edb
Manavendra Sen <manavendra4288@gmail.com>
2024-06-23 22:42:50 +0530

    feat: add split kotlin service

---
+++ split-service/src/main/kotlin/com/manavendrasen/splitpay/SplitpayApplication.kt
@@ -0,0 +1,11 @@
+package com.manavendrasen.splitpay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplitpayApplication
+
+fun main(args: Array<String>) {
+	runApplication<SplitpayApplication>(*args)
+}
